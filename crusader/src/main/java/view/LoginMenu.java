cbd83416e3c4d4e8fcb8937475fa3d9c58579efb
mitra gholipoor <mitra9595gh@gmail.com>
2023-04-19 20:29:56 +0330

    test

--- crusader/src/main/java/view/LoginMenu.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-import java.util.Scanner;
-
-public class LoginMenu {
-    public static void run(Scanner scanner){
-
-    }
-}
--- crusader/src/main/java/view/MainMenu.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-import java.util.Scanner;
-
-public class MainMenu {
-    public static void run(Scanner scanner) {
-
-    }
-}
