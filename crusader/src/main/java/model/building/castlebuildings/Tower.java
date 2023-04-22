217a8190ae030b78ddad16bf7483f5bc8240b8bc
FKR1383 <FarzamProgrammer@gmail.com>
2023-04-22 16:44:12 +0430

    Towers added.

--- crusader/src/main/java/model/building/castlebuildings/Tower.java
+++ crusader/src/main/java/model/building/castlebuildings/Tower.java
@@ -3,8 +3,30 @@ package model.building.castlebuildings;
 import model.Government;
 
 public class Tower extends CastleBuilding {
+    // 1000 for smallStoneCastle 10
+    // 2000 for bigStoneCastle 20
+    // 250 for lookout 10 ---- 60
+    // 1000 for perimeter turret 10 ---- 40
+    // 1200 for defense turret 15 ---- 40
+    // 1600 for square tower 35 ---- 50
+    // 2000 for round tower 40 ---- 50
+    // 0 for walls 1
+    // 0 for drawbridge 10 woods
+
+
+
     private int fireRange;
     private int defendRange;
+    private String typeOfTower;
+    private boolean canKeepRidingEquipment;
+
+    public boolean isCanKeepRidingEquipment() {
+        return this.canKeepRidingEquipment;
+    }
+
+    public void setCanKeepRidingEquipment(boolean canKeepRidingEquipment) {
+        this.canKeepRidingEquipment = canKeepRidingEquipment;
+    }
 
     public Tower(int numberOfRequiredWorkers, int numberOfRequiredEngineers, String type, int maxHp,
                  int width, int length, int fireRange, int defendRange) {
