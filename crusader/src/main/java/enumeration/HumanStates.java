1d28798a22cb85eeb27d8eab43a2f0dfe135c842
mitra gholipoor <mitra9595gh@gmail.com>
2023-05-01 13:19:35 +0330

    human state enum

--- crusader/src/main/java/enumeration/HumanStates.java
+++ crusader/src/main/java/enumeration/HumanStates.java
@@ -1,4 +1,17 @@
 package enumeration;
 
 public enum HumanStates {
+
+    STAND_GROUND("standing"),
+    DEFENSIVE_STANCE("defensive"),
+    AGGRESSIVE_STANCE("offensive");
+    private final String state;
+
+    private HumanStates(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
