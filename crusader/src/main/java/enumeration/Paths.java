cfbfa4acfa88f700f719ec34c8b535f5a1127a5e
FKR1383 <FarzamProgrammer@gmail.com>
2023-05-12 14:13:11 +0430

    an error solved

--- crusader/src/main/java/enumeration/Paths.java
+++ crusader/src/main/java/enumeration/Paths.java
@@ -2,17 +2,17 @@ package enumeration;
 
 
 public enum Paths {
-    USERS_PATH("files/user/users.json"),
-    CURRENT_USER_PATH("files/user/currentUser.json"),
-    CAPTCHA_DATA_PATH("files/captcha/data/"),
-    GOODS_PATH("files/model/goods/goods.json"),
-    FOODS_PATH("files/model/goods/foods.json"),
-    RESOURCES_PATH("files/model/goods/resources.json"),
-    WEAPONS_PATH("files/model/goods/weapons.json"),
-    EUROPEAN_TROOP_PATH("files/model/humans/europeanTroops.json"),
-    ARABIAN_MERCENARY_PATH("files/model/humans/arabianMercenaries.json"),
-    BUILDINGS_PATH("files/model/buildings/"),
-    TOOLS_PATH("files/model/tools/tools.json"),
+    USERS_PATH("crusader/files/user/users.json"),
+    CURRENT_USER_PATH("crusader/files/user/currentUser.json"),
+    CAPTCHA_DATA_PATH("crusader/files/captcha/data/"),
+    GOODS_PATH("crusader/files/model/goods/goods.json"),
+    FOODS_PATH("crusader/files/model/goods/foods.json"),
+    RESOURCES_PATH("crusader/files/model/goods/resources.json"),
+    WEAPONS_PATH("crusader/files/model/goods/weapons.json"),
+    EUROPEAN_TROOP_PATH("crusader/files/model/humans/europeanTroops.json"),
+    ARABIAN_MERCENARY_PATH("crusader/files/model/humans/arabianMercenaries.json"),
+    BUILDINGS_PATH("crusader/files/model/buildings/"),
+    TOOLS_PATH("crusader/files/model/tools/tools.json"),
     ;
     private final String path;
 
