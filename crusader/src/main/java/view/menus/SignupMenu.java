59db118303caa715d501bb3ed35fcdface66bc2e
Amir <amirhsnsouri@gmail.com>
2023-05-24 19:48:06 +0330

    some changes !!

--- crusader/src/main/java/view/menus/SignupMenu.java
+++ crusader/src/main/java/view/menus/SignupMenu.java
@@ -1,11 +1,21 @@
 package view.menus;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class SignupMenu extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        GridPane signupPane = FXMLLoader.load(SignupMenu.class.getResource("/FXML/signupMenu.fxml"));
+        Scene scene = new Scene(signupPane);
 
+
+
+        stage.setTitle("Signup Menu");
+        stage.setScene(scene);
+        stage.show();
     }
 }
