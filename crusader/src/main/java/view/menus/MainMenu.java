ac3aaf0798e3aeac86c09eaa80200c8ae848c400
mitra gholipoor <mitra9595gh@gmail.com>
2023-05-25 10:33:23 +0330

    test py

--- crusader/src/main/java/view/menus/MainMenu.java
+++ crusader/src/main/java/view/menus/MainMenu.java
@@ -4,12 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import view.controllers.ViewController;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Objects;
 
@@ -33,11 +34,24 @@ public class MainMenu extends Application {
         BorderPane pane = FXMLLoader.load(
                 new URL(Objects.requireNonNull(LoginMenu.class.getResource("/fxml/mainMenu.fxml")).toExternalForm()));
 
-        root = ViewController.makeScreen(stage,pane,1000,-1);
+        root = ViewController.makeScreen(stage, pane, 1000, -1);
 
 
         Scene scene = new Scene(pane);
         stage.setScene(scene);
         stage.show();
+        runPy();
+
+    }
+
+    public void runPy() {
+        try {
+            Process p = Runtime.getRuntime().exec("python files/captcha/data/main.py");
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            int ret = new Integer(in.readLine());
+            System.out.println(ret);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
