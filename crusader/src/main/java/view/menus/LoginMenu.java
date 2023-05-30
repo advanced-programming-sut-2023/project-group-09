a331121b7f12ba2caff7c0eb9930378a86b95ef2
FKR1383 <FarzamProgrammer@gmail.com>
2023-05-30 21:27:47 +0330

    login menu first try.

--- crusader/src/main/java/view/menus/LoginMenu.java
+++ crusader/src/main/java/view/menus/LoginMenu.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import model.captcha.Captcha;
 import view.controllers.CaptchaController;
@@ -16,16 +17,17 @@ import java.net.MalformedURLException;
 
 public class LoginMenu extends Application {
     public static Stage stage;
+    public StackPane captchaStackPane;
+
     @Override
     public void start(Stage stage) throws Exception {
         LoginMenu.stage = stage;
         BorderPane loginPane = FXMLLoader.load(LoginMenu.class.getResource("/FXML/loginMenu.fxml"));
         Scene scene = new Scene(loginPane);
         Captcha captcha = new Captcha();
-        loginPane.getChildren().add(captcha);
-        CaptchaController.getCaptcha().getRefreshButton().requestFocus();
+        captchaStackPane = captcha;
 
-        stage.setTitle("Signup Menu");
+        stage.setTitle("Login Menu");
         stage.setScene(scene);
 
 
@@ -33,6 +35,9 @@ public class LoginMenu extends Application {
     }
 
 
+    public void forgotPassword(MouseEvent mouseEvent) {
+    }
 
-
+    public void login(MouseEvent mouseEvent) {
+    }
 }
