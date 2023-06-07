0acbb6482a6d660ca0c10a1bd9765e4f5c5aae81
Amir <amirhsnsouri@gmail.com>
2023-06-07 07:20:21 +0330

    add changes

--- crusader/src/main/java/model/menugui/game/GameTile.java
+++ crusader/src/main/java/model/menugui/game/GameTile.java
@@ -1,15 +1,11 @@
 package model.menugui.game;
 
 import enumeration.Paths;
-import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import model.building.Building;
 import model.game.Tile;
-import org.w3c.dom.ls.LSOutput;
-import view.controllers.GameViewController;
 
 public class GameTile extends StackPane {
     private Tile tile;
@@ -21,8 +17,7 @@ public class GameTile extends StackPane {
     private double height;
     private ImageView textureImage;
     private ImageView buildingImage;
-    private Image textureImg;
-    private static int tileXOn , tileYOn;
+    private static int tileXOn, tileYOn;
 
     public GameTile(Tile tile, double x, double y, int tileX, int tileY) {
         this.tileX = tileXOn = tileX;
@@ -57,45 +52,31 @@ public class GameTile extends StackPane {
     }
 
     public void setBuilding() {
-        if (tileX == 10 && tileY == 20) {
-            Building building = tile.getBuilding();
-            textureImage.setImage(new Image(GameTile.class.getResource(Paths.MAP_IMAGES.getPath()
-                    + "textures/red.png").toExternalForm()));
-            buildingImage = new ImageView(new Image(GameTile.class.getResource(Paths.MAP_IMAGES.getPath()
-                    + "buildings/stockPile.png").toExternalForm()));
-            buildingImage.setTranslateX(x);
-            buildingImage.setTranslateY(y - buildingImage.getImage().getHeight() / 2 + textureImage.getFitHeight() / 2);
-//            buildingImage.setViewOrder(-1);
-            this.setViewOrder(-1);
-            this.getChildren().add(buildingImage);
-        }
-
-        if (tileX == 20 && tileY == 20) {
-            Building building = tile.getBuilding();
-//            textureImage.setImage(new Image(GameTile.class.getResource(Paths.MAP_IMAGES.getPath()
-//                    + "textures/red.png").toExternalForm()));
-            buildingImage = new ImageView(new Image(GameTile.class.getResource(Paths.MAP_IMAGES.getPath()
-                    + "buildings/" + tile.getBuilding().getName() + ".png").toExternalForm()));
-            buildingImage.setTranslateX(x - buildingImage.getImage().getWidth() * (building.getWidth() - building.getLength())
-            / (building.getWidth() + building.getLength()) / 2);
-            buildingImage.setTranslateY(y - buildingImage.getImage().getHeight() / 2 + textureImage.getFitHeight() / 2);
-//            buildingImage.setViewOrder(-1);
-            this.setViewOrder(-1);
-            this.getChildren().add(buildingImage);
-        }*/
+//        if (tileX == 20 && tileY == 20) {
+//            Building building = tile.getBuilding();
+////            textureImage.setImage(new Image(GameTile.class.getResource(Paths.MAP_IMAGES.getPath()
+////                    + "textures/red.png").toExternalForm()));
+//            buildingImage = new ImageView(new Image(GameTile.class.getResource(Paths.MAP_IMAGES.getPath()
+//                    + "buildings/" + tile.getBuilding().getName() + ".png").toExternalForm()));
+//            buildingImage.setTranslateX(x - buildingImage.getImage().getWidth() * (building.getWidth() - building.getLength())
+//                    / (building.getWidth() + building.getLength()) / 2);
+//            buildingImage.setTranslateY(y - buildingImage.getImage().getHeight() / 2 + textureImage.getFitHeight() / 2);
+////            buildingImage.setViewOrder(-1);
+//            this.setViewOrder(-1);
+//            this.getChildren().add(buildingImage);
+//        }
         Building building = tile.getBuilding();
-        if (building != null) {
+        if (building != null && building.getEndX() == tileX && building.getEndY() == tileY) {
             Image image = new Image(GameTile.class.getResource(Paths.MAP_IMAGES.getPath()
                     + "buildings/" + building.getName() + ".png").toExternalForm());
             buildingImage = new ImageView(image);
-            buildingImage.setTranslateX(x + (image.getWidth() *
-                    ((double)building.getLength() - building.getWidth())/(building.getLength() + building.getWidth())));
-            buildingImage.setTranslateY(y - image.getHeight()/2 + textureImg.getHeight()/4);
+            buildingImage.setTranslateX(x + image.getWidth() * ((double) building.getLength() - building.getWidth())
+                    / (building.getLength() + building.getWidth()) / 2);
+            buildingImage.setTranslateY(y - image.getHeight() / 2 + textureImage.getImage().getHeight() / 2);
 //            buildingImage.setViewOrder(-1);
             this.setViewOrder(-1);
             this.getChildren().add(buildingImage);
         }
-
     }
 
     public int getTileX() {
