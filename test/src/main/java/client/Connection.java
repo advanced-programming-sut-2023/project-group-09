202558c28d312a0409a8a3c64d2ababc353f9e9c
mitra gholipoor <mitra9595gh@gmail.com>
2023-06-28 16:00:05 +0330

    ,,,,

---
+++ test/src/main/java/client/Connection.java
@@ -0,0 +1,35 @@
+package client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Connection {
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+
+    public Connection(String host, int port) throws IOException {
+        System.out.println("Starting Client service...");
+        Socket socket = new Socket(host, port);
+        dataInputStream = new DataInputStream(socket.getInputStream());
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+    }
+
+    public DataInputStream getDataInputStream() {
+        return dataInputStream;
+    }
+
+    public void setDataInputStream(DataInputStream dataInputStream) {
+        this.dataInputStream = dataInputStream;
+    }
+
+    public DataOutputStream getDataOutputStream() {
+        return dataOutputStream;
+    }
+
+    public void setDataOutputStream(DataOutputStream dataOutputStream) {
+        this.dataOutputStream = dataOutputStream;
+    }
+}
---
+++ test/src/main/java/client/Packet.java
@@ -0,0 +1,29 @@
+package client;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import controller.DBController;
+import view.Main;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Packet {
+    public String command;
+    public HashMap<String , Object> attributes = new HashMap<>();
+
+    public Packet(String command) {
+        this.command = command;
+    }
+
+    public void addAttribute(String key , Object value) {
+        this.attributes.put(key , value);
+    }
+
+    public void sendPacket() throws IOException {
+        String json = new GsonBuilder().setPrettyPrinting().create().toJson(this);
+        Main.connection.getDataOutputStream().writeUTF(json);
+    }
+}
