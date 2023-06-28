bccef0a77246d307c9603c3a57d7dee51a028b89
Amir <amirhsnsouri@gmail.com>
2023-06-28 17:14:12 +0330

    add PacketHandler

---
+++ test/src/main/java/client/PacketHandler.java
@@ -0,0 +1,15 @@
+package client;
+
+public class PacketHandler {
+    Packet packet;
+
+    public PacketHandler(Packet packet) {
+        this.packet = packet;
+    }
+
+    public void handle() {
+        switch (packet.command) {
+
+        }
+    }
+}
