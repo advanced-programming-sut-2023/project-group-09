34c8c53640b3e4f825bb34baf577ca8cdd99e981
mitra gholipoor <mitra9595gh@gmail.com>
2023-06-28 23:29:56 +0330

    ...

--- crusader/src/main/java/server/PacketHandler.java
+++ crusader/src/main/java/server/PacketHandler.java
@@ -4,17 +4,18 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import controller.Application;
 import controller.DBController;
+import controller.TokenController;
 import controller.UserController;
 import model.User;
-import model.User;
-import view.Main;
+import server.handlers.ProfileHandler;
+import server.handlers.UserHandler;
 
 import java.io.IOException;
 
 public class PacketHandler {
     Packet packet;
     Connection connection;
-    Connection connection;
+
 
     public PacketHandler(Packet packet, Connection connection) {
         this.packet = packet;
@@ -34,23 +35,13 @@ public class PacketHandler {
 
 
     public void handle() throws IOException {
-        if (!validateAuthenticationToken()){
-            return;
-        }
-
-        switch (packet.handler) {
-            case "profile":
-                new ProfileHandler().handle(packet,connection);
-                break;
-            case "user":
-                new UserHandler().handle(packet,connection);
-        }
         switch (packet.command) {
             case "login user" -> {
                 Packet result = UserController.loginUser((String) packet.getAttribute("username"),
                         (String) packet.getAttribute("password"),
                         (boolean) packet.getAttribute("stayedLoggedIn"));
                 sendPacket(result);
+                return;
             }
             case "email validation" -> {
 
@@ -59,6 +50,7 @@ public class PacketHandler {
                     validateEmail.addAttribute("error", "email already exists!");
                 else validateEmail.addAttribute("success", "");
                 sendPacket(validateEmail);
+                return;
             }
             case "username validation" -> {
                 Packet validateUsername = new Packet("username validation");
@@ -66,36 +58,52 @@ public class PacketHandler {
                     validateUsername.addAttribute("error", "username already exists!");
                 else validateUsername.addAttribute("success", "valid username");
                 sendPacket(validateUsername);
+                return;
             }
             case "signup user" -> {
                 Application.addUser(new Gson().fromJson((String) packet.getAttribute("user"), User.class));
                 DBController.saveAllUsers();
+                return;
             }
             case "is username exists?" -> {
-                Packet result = UserController.isUserExists((String)packet.getAttribute("username"));
+                Packet result = UserController.isUserExists((String) packet.getAttribute("username"));
                 sendPacket(result);
+                return;
             }
             case "send user" -> {
-                User user = Application.getUserByUsername((String)packet.getAttribute("username"));
+                User user = Application.getUserByUsername((String) packet.getAttribute("username"));
                 sendUser(user);
                 String input = connection.getDataInputStream().readUTF();
                 if (!input.equals("do nothing!")) {
                     Application.getUsers().remove(user);
-                    user = new GsonBuilder().setPrettyPrinting().create().fromJson(input , User.class);
+                    user = new GsonBuilder().setPrettyPrinting().create().fromJson(input, User.class);
                     Application.getUsers().add(user);
                     DBController.saveAllUsers();
                 }
+                return;
             }
         }
+        if (!validateAuthenticationToken()) {
+            return;
+        }
+        switch (packet.handler) {
+            case "profile":
+                new ProfileHandler().handle(packet, connection);
+                break;
+            case "user":
+                new UserHandler().handle(packet, connection);
+        }
+
     }
+
     public boolean validateAuthenticationToken() throws IOException {
-        if (packet.handler.equals("login") || packet.handler.equals("register")|| packet.command.equals("make a fake token")){
+        if (packet.handler.equals("login") || packet.handler.equals("register") || packet.command.equals("make a fake token")) {
             return true;
         }
         boolean check = TokenController.validateToken(packet.token);
-        if (!check){
-            Packet packet = new Packet("authentication error","error");
-            Packet.sendPacket(packet,connection);
+        if (!check) {
+            Packet packet = new Packet("authentication error", "error");
+            Packet.sendPacket(packet, connection);
         }
         return check;
     }
